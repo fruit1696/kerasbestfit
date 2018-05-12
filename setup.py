975f84333b20efc3af696f87438043c6155ca97e
Russ Beuker <russbeuker@gmail.com>
2018-05-11 20:52:44 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'kerasbestfit',
+  packages = ['kerasbestfit'],
+  version = '0.1',
+  description = 'For finding the best model using Keras.',
+  author = 'Russ Beuker',
+  author_email = 'russbeuker@gmail.com',
+  url = 'https://github.com/russbeuker/kerasbestfit',
+  download_url = 'https://github.com/russbeuker/kerasbestfit/archive/0.1.tar.gz',
+  keywords = ['keras', 'fit', 'best'], # arbitrary keywords
+  classifiers = [],
+)
