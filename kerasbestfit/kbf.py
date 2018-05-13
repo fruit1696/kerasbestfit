735763af16a44345dbbc18f98200ed6e58941a09
Russ Beuker <russbeuker@gmail.com>
2018-05-12 22:59:44 -0700

    Added max_duration_mins parameter to fbf.fit function to allow stopping after a certain number of minutes.

--- kerasbestfit/kbf.py
+++ kerasbestfit/kbf.py
@@ -1,9 +1,12 @@
 from keras.callbacks import EarlyStopping, Callback
+import datetime
 
 class _FBFCheckpoint(Callback):
     def __init__(self, metric='val_acc', save_best=False, save_path=None, best_val_acc_so_far=0.0, snifftest_max_epoch=0, snifftest_min_val_acc=-1.0,
-                 progress_callback=None, show_progress=True, format_val_acc='{:1.10f}'):
+                 progress_callback=None, show_progress=True, format_val_acc='{:1.10f}', started_at=0.0, finish_by=0.0):
         super().__init__()
+        self.started_at = started_at
+        self.finish_by = finish_by
         self.save_best = save_best
         self.save_path = save_path
         self.previous_best_val_acc = best_val_acc_so_far
@@ -25,57 +28,63 @@ class _FBFCheckpoint(Callback):
         self.show_progress = show_progress
         self.format_val_acc = format_val_acc
         self.metric = metric
+        self.expired = False
 
     def on_epoch_end(self, epoch, logs=()):
-        logs = logs or {}
-        logs['epoch'] = epoch
-        self.full_log.append(logs)
-        self.saved_at_epoch = False
-        self.is_best = False
-        self.saved = False
-        self.current_epoch = epoch
-        self.current_epoch_val_acc = logs.get('val_acc')
-        self.snifftest_failed = (self.current_epoch >= self.snifftest_max_epoch) and (
-        self.snifftest_min_val_acc >= self.current_epoch_val_acc)
-        self.model.stop_training = self.snifftest_failed
+        if not self.expired:
+            logs = logs or {}
+            logs['epoch'] = epoch
+            self.full_log.append(logs)
+            self.saved_at_epoch = False
+            self.is_best = False
+            self.saved = False
+            self.current_epoch = epoch
+            self.current_epoch_val_acc = logs.get('val_acc')
+            self.snifftest_failed = (self.current_epoch >= self.snifftest_max_epoch) and (
+            self.snifftest_min_val_acc >= self.current_epoch_val_acc)
+            self.model.stop_training = self.snifftest_failed
+            if self.snifftest_failed == False:
+                if self.current_epoch_val_acc > self.best_val_acc:
+                    self.best_val_acc = self.current_epoch_val_acc
+                    self.best_epoch = epoch
+                    self.is_best = True
 
-        if self.snifftest_failed == False:
-            if self.current_epoch_val_acc > self.best_val_acc:
-                self.best_val_acc = self.current_epoch_val_acc
-                self.best_epoch = epoch
-                self.is_best = True
+                    if self.best_val_acc > self.best_val_acc_so_far:
+                        self.previous_best_val_acc = self.best_val_acc_so_far
+                        self.best_val_acc_so_far = self.best_val_acc
 
-                if self.best_val_acc > self.best_val_acc_so_far:
-                    self.previous_best_val_acc = self.best_val_acc_so_far
-                    self.best_val_acc_so_far = self.best_val_acc
+                        #save model structure as .json and weights as .hdf5 only if snifftest has passed
+                        if (self.current_epoch >= self.snifftest_max_epoch):
+                            if self.save_best:
+                                self.saved = True
+                                self.saved_at_epoch = self.best_epoch
+                                self.saved_at_val_acc = self.best_val_acc
+                                model_json = self.model.to_json()
+                                with open(self.save_path + '.json', "w") as json_file:
+                                    json_file.write(model_json)
+                                self.model.save_weights(self.save_path + '.hdf5')
 
-                    #save model structure as .json and weights as .hdf5 only if snifftest has passed
-                    if (self.current_epoch >= self.snifftest_max_epoch):
-                        if self.save_best:
-                            self.saved = True
-                            self.saved_at_epoch = self.best_epoch
-                            self.saved_at_val_acc = self.best_val_acc
-                            model_json = self.model.to_json()
-                            with open(self.save_path + '.json', "w") as json_file:
-                                json_file.write(model_json)
-                            self.model.save_weights(self.save_path + '.hdf5')
+            if self.show_progress:
+                cva = self.format_val_acc.format(self.current_epoch_val_acc)
+                bsf = self.format_val_acc.format(self.best_val_acc_so_far)
+                is_best_so_far = self.is_best and (self.best_val_acc_so_far > self.previous_best_val_acc)
+                flags = '  '
+                msg = ''
+                if self.is_best:
+                    flags = '* '
+                if is_best_so_far:
+                    flags = '*!'
+                if self.saved:
+                    msg = ' Saved '
+                if self.snifftest_failed:
+                    msg = ' Snifftest failed '
+                print(f'  e{self.current_epoch}: {self.metric}={cva} {flags} bsf={bsf} {msg}')
 
-        if self.show_progress:
-            cva = self.format_val_acc.format(self.current_epoch_val_acc)
-            bsf = self.format_val_acc.format(self.best_val_acc_so_far)
-            is_best_so_far = self.is_best and (self.best_val_acc_so_far > self.previous_best_val_acc)
-            flags = '  '
-            msg = ''
-            if self.is_best:
-                flags = '* '
-            if is_best_so_far:
-                flags = '*!'
-            if self.saved:
-                msg = ' Saved '
-            if self.snifftest_failed:
-                msg = ' Snifftest failed '
-            print(f'  e{self.current_epoch}: {self.metric}={cva} {flags} bsf={bsf} {msg}')
-
+            if (self.finish_by != 0) and (datetime.datetime.today() >= self.finish_by):
+                fmt = "%a %b %d %H:%M:%S %Y"
+                print(f'  Finish_by time has been reached.  Fit terminated at {self.finish_by.strftime(fmt)}')
+                self.model.stop_training = True
+                self.expired = True
 
 # -----------------------------------------------------------------------------------------------------------------------
 def find_best_fit(
@@ -96,9 +105,15 @@ def find_best_fit(
         progress_val_acc_format='{:1.10f}',
         save_best=False,
         save_path=None,
-        previous_best_val_acc=0,
-        best_val_acc_so_far=0
+        best_val_acc_so_far=0,
+        started_at=0,
+        finish_by=0
         ):
+    #started_at - (datetime) set this to the time you are starting a training session.
+    #finish_by - (float) in minutes(ex: 120.0 for 2.0 hours, 2.0 for 2 minutes, 0.25 for 25 seconds.  Training will expire started_by + finish_by.
+    #   is tested after epoch completion, so if you have a time limit, plan it for time limit minus the time to complete a single epoch, so leave
+    #   yourself some extra time for finishing.  Example, I have a hard total training limit of 6 hours, so I'll set the finish_by to 6*60-10 where
+    #   the 10 minutes is just be be sure that the last epoch completes before the time is up.
 
     cbstopper = EarlyStopping(monitor='val_acc', patience=patience, verbose=0)
     cbcheckpoint = _FBFCheckpoint(save_best=save_best,
@@ -108,7 +123,9 @@ def find_best_fit(
                                  snifftest_max_epoch=snifftest_max_epoch,
                                  snifftest_min_val_acc=snifftest_min_val_acc,
                                  show_progress=show_progress,
-                                 format_val_acc=progress_val_acc_format)
+                                 format_val_acc=progress_val_acc_format,
+                                 started_at=started_at,
+                                 finish_by=finish_by)
 
     # call the native fit function
     if validation_split == 0:
@@ -130,6 +147,7 @@ def find_best_fit(
                             validation_split=validation_split)
 
     results = {}
+    results['expired'] = cbcheckpoint.expired
     results['snifftest_failed'] = cbcheckpoint.snifftest_failed
     results['is_best'] = cbcheckpoint.is_best
     results['saved'] = cbcheckpoint.saved
