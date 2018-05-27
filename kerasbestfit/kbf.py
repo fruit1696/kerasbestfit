79333ff2b080667661166bd1180156eebdbeb164
Russ Beuker <russbeuker@gmail.com>
2018-05-27 12:52:51 -0700

    - updated parameter comments

--- kerasbestfit/kbf.py
+++ kerasbestfit/kbf.py
@@ -144,11 +144,49 @@ def find_best_fit(
         logmsg_callback=None,
         progress_callback=None
         ):
-    #started_at - (datetime) set this to the time you are starting a training session.
-    #finish_by - (float) in minutes(ex: 120.0 for 2.0 hours, 2.0 for 2 minutes, 0.25 for 25 seconds.  Training will expire started_by + finish_by.
+    # model - a compiled Keras model
+    # metric - (string) - use 'val_acc' or 'val_loss'.  It will search for the best value for this metric.
+    # xtrain - (numpy array) - This is passed to the Keras .fit function.  See Keras documentation for more info.
+    # ytrain - (numpy array) - This is passed to the Keras .fit function.  See Keras documentation for more info.
+    # xval - (numpy array) - This is passed to the Keras .fit function.  See Keras documentation for more info.
+    # yval - (numpy array) - This is passed to the Keras .fit function.  See Keras documentation for more info.
+    # validation_split  - (float) - set to non-zero value to split the xtrain and ytrain into additional validation
+    #   datasets. If non-zero, the xval and yval params are ignored.
+    #   This is passed to the Keras .fit function.  See Keras documentation for more info.
+    # shuffle - (boolean) - use this to randomly select the validation split.  Only works if validation_split is not zero.
+    #   This is passed to the Keras .fit function.  See Keras documentation for more info.
+    # batch_size - (integer) - this is passed to the Keras .fit function.  See Keras documentation for more info.
+    # epochs - (integer) - this is passed to the Keras .fit function.  See Keras documentation for more info.
+    # patience - (integer) - this is passed to the Keras early stopping callback.  See Keras documentation for more info.
+# snifftest_max_epoch - (integer) - set this to the epoch in which the snifftest_metric_val must be reached or else
+    #   the training will be aborted.  Great for hyperparameter searchs where some parameters give recognizably horrible results.
+    #   set to zero to disable the snifftest
+    # snifftest_metric_val - (float) - set this to the metric value that must be reached by snifftest_max_epoch epochs.
+    #   set to zero to disable the snifftest
+    # show_progress - (boolean) - set to True to see training log messages.
+    # format_metric_val - string) - this is the formatting used for formatting floats for training logging.
+    # save_best - (boolean) - set this to True to cause the model to be saved when a new best metric val is found
+    # save_path = (string) - the folder that the saved model will be saved to.  The model files will always be named
+    #  'model' and give files model.json and model.hdf5
+    #   EXAMPLE
+    #   ".//models//
+    # best_metric_val_so_far - (float) - use this to keep track which is the best metric val found so far.  This is
+    #   use in the output messages to denote the ! symbol.
+    # started_at - (datetime) set this to the time you are starting a training session.
+    # finish_by - (float) in minutes(ex: 120.0 for 2.0 hours, 2.0 for 2 minutes, 0.25 for 25 seconds.  Training will expire started_by + finish_by.
     #   is tested after epoch completion, so if you have a time limit, plan it for time limit minus the time to complete a single epoch, so leave
     #   yourself some extra time for finishing.  Example, I have a hard total training limit of 6 hours, so I'll set the finish_by to 6*60-10 where
     #   the 10 minutes is just be be sure that the last epoch completes before the time is up.
+    # logmsg_callback - (function) - a function that will be used for displaying status messages.  Requires a string parameter.
+    #   EXAMPLE
+    #   def log(msg=''):
+    #       print(msg)
+    #       return
+    # progress_callback - (function) - a function that is called at the end of each epoch and contains training log data
+    #   EXAMPLE
+    #   def on_progress(epoch, acc, loss, val_acc, val_loss):
+    #       print(epoch, acc, loss, val_acc, val_loss)
+    #       return
 
     cbstopper = EarlyStopping(monitor=metric, patience=patience, verbose=0)
     cbcheckpoint = _FBFCheckpoint(save_best=save_best,
@@ -196,4 +234,3 @@ def find_best_fit(
     results['history'] = history.history
 
     return results, cbcheckpoint.full_log
-
